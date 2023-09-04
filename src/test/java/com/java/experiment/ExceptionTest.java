c7854db7c51b85ae0d9ea100878ad9e488fc91ba
eric <liueric044@gmail.com>
2023-09-04 13:23:11 +0800

    add test for try catch exception

---
+++ src/test/java/com/java/experiment/ExceptionTest.java
@@ -0,0 +1,27 @@
+package com.java.experiment;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+
+@Slf4j
+public class ExceptionTest {
+
+    @Test
+    void Test_try_exception(){
+        try{
+            String s = null;
+            s.toString();
+        }
+        catch(NullPointerException npe){
+            log.error("NullPointerException error cause:{} error message:{} ",
+                    npe.getCause(), npe.getMessage(), npe);
+        }
+        catch(Exception e){
+            log.error("Exception error cause:{} error message:{} ",
+                    e.getCause(), e.getMessage(), e);
+        }finally {
+            log.info("finally");
+        }
+    }
+}
